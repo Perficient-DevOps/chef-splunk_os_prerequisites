7bea3b7c735b01bc24611e804b8232799f82490d
Sean G Wilbur <sgwilbur@gmail.com>
2018-04-16 13:28:10 -0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# splunk_os_prerequisites
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: splunk_os_prerequisites
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
